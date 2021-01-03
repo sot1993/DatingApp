deb956b7088c5d15b8cf0d0b31e688c9aefbb904
sot1993 <49124176+sot1993@users.noreply.github.com>
2021-01-04 00:07:51 +0200

    End of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -21,6 +21,7 @@ export class AccountService {
         const user = response;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
       })
     );
